334d67695d6cb374b036ccc2e68b4c12a956dd72
jolo <57520402+jolo61100@users.noreply.github.com>
2020-07-10 13:37:08 +0800

    added waves

--- Hotline Bilibid/Assets/Scripts/gameplayScript.cs
+++ Hotline Bilibid/Assets/Scripts/gameplayScript.cs
@@ -9,18 +9,37 @@ public class gameplayScript : MonoBehaviour
     [Header("UI")]
     [SerializeField] TextMeshProUGUI _timer;
     [SerializeField] float _timeToNextWave = 10.0f;
-    public static bool _spawnWave = false;
+    float _currentTime;
+    [Header("SPAWN")]
+    [SerializeField] GameObject[] _objectsEnemy;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _currentTime = _timeToNextWave;
+        StartCoroutine(game());
+        SpawnEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _currentTime -= Time.deltaTime;
+        _timer.text = "NEXT WAVE: " + (int)_currentTime;
+    }
 
+    public IEnumerator game()
+    {
+        yield return new WaitForSeconds(_timeToNextWave);
+        _currentTime = _timeToNextWave;
+        SpawnEnemy();
+        StartCoroutine(game());
+    }
+
+    void SpawnEnemy()
+    {
+        int _rand = Random.Range(0, _objectsEnemy.Length);
+        Instantiate(_objectsEnemy[_rand], transform.position, Quaternion.identity);
     }
 }
--- Hotline Bilibid/Assets/Scripts/spawnerScript.cs
+++ Hotline Bilibid/Assets/Scripts/spawnerScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class spawnerScript : MonoBehaviour
 {
     [SerializeField] GameObject[] _objects;
+    //[SerializeField] gameplayScript controller;
+
     private void Start()
     {
+        // /StartCoroutine(controller.game());
         Spawn();
     }
+
     void Spawn()
     {
         int _rand = Random.Range(0, _objects.Length);
