8246b266a8f5d4b5c591c88ea4395c8af1f15df3
Jolo <jgutierrez1100@gmail.com>
2020-07-13 15:21:15 +0800

    merged russel's code

---
+++ Hotline Bilibid/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!!!");
+        PlayerPrefs.DeleteAll();
+        Application.Quit();
+    }
+}
